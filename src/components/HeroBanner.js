87c3d1db8e8966f62bd958b4744d5613e47e0389
panditdhamdhere <dhamdherepandit@gmail.com>
2023-05-04 17:05:21 +0530

    navbar adding

---
+++ src/components/HeroBanner.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Box, Stack, Typography } from "@mui/material";
+
+const HeroBanner = () => {
+  return (
+    <Box 
+    sx={{ 
+      mt: { lg: "212px", sx: '70px' },
+      ml: { sm: "50px"}
+      }}>
+      <Typography color="#FF2625" fontWeight="600" fontSize="26px">
+        Fitness Club
+      </Typography>
+    </Box>
+  );
+};
+
+export default HeroBanner;
